50ee7f27987943118ca63b4e25048a4d7cc1f33d
DESKTOP-NI72LT7\selecty <andrey.sudomoykin@gmail.com>
2021-11-28 19:57:56 +0300

    добавлено несколько функций

--- sandbox/build/scripts/sandbox.bat
+++ sandbox/build/scripts/sandbox.bat
@@ -71,7 +71,7 @@ set CLASSPATH=%APP_HOME%\lib\sandbox.jar
 
 
 @rem Execute sandbox
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" MyFirstProgramm %*
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %SANDBOX_OPTS%  -classpath "%CLASSPATH%" ru.stqa.pft.sandbox.MyFirstProgram %*
 
 :end
 @rem End local scope for the variables with windows NT shell
--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,8 +3,26 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] arg) {
-        System.out.println("Hello, World!");
+        hello("wolrd");
+        hello("user");
+        hello ("Andy");
 
+        double l = 6;
+        System.out.println("Площадь кварата со стороной " +l + "=" +aria(l));
+
+        double q = 3;
+        double k = 6;
+        System.out.println("Площадь прямоуголника с сторонами " + q +" и " + k + " = " +aria(q,k));
+    }
+    public static void hello(String somebody){
+        System.out.println("Hello, " + somebody +"!");
     }
 
+    public static double aria(double len){
+        return len * len;
+    }
+
+    public static double aria(double a, double b){
+        return  a * b;
+    }
 }
