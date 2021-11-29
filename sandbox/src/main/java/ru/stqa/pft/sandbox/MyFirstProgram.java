4f92d597d6c3ef3a8b82babcf801df558a9a554d
DESKTOP-NI72LT7\selecty <andrey.sudomoykin@gmail.com>
2021-11-29 19:01:22 +0300

    добавлены объекты и методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,27 +2,21 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-    public static void main(String[] arg) {
-        hello("wolrd");
-        hello("user");
-        hello ("Andy");
+  public static void main(String[] arg) {
+    hello("world");
+    hello("user");
+    hello("Andy");
 
-        double l = 6;
-        System.out.println("Площадь кварата со стороной " +l + "=" +aria(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + "=" + s.aria());
 
-        double q = 3;
-        double k = 6;
-        System.out.println("Площадь прямоуголника с сторонами " + q +" и " + k + " = " +aria(q,k));
-    }
-    public static void hello(String somebody){
-        System.out.println("Hello, " + somebody +"!");
-    }
+    Rectangle r = new Rectangle(2, 5);
+    System.out.println("Площадь прямоугольника с сторонами " + r.a + " и " + r.b + " = " + r.aria());
+  }
 
-    public static double aria(double len){
-        return len * len;
-    }
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
 
-    public static double aria(double a, double b){
-        return  a * b;
-    }
+
 }
