1423f8a782ab1d483330b61dfdfc790fca3165ff
DESKTOP-NI72LT7\selecty <andrey.sudomoykin@gmail.com>
2021-12-06 20:27:06 +0300

    Фикстуры, перенесли шаги входа а БД в блок BeforeTest
    Фикстуры: BeforeTest & AfterTest - то что зажимает, фиксирует тест между началом и завершением теста

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -1,8 +1,10 @@
 package ru.stqa.pft.addressbook;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -16,20 +18,20 @@ public class GroupCreationTests {
   public void setUp() throws Exception{
     System.setProperty("webdriver.gecko.driver","c:/temp/geckodriver.exe");
     //    WebDriver driver = new FirefoxDriver();
-
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-      }
-  @Test
-  public void GroupCreationTests() {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.name("user")).click();
- //   wd.findElement(By.name("user")).clear();
+    //   wd.findElement(By.name("user")).clear();
     wd.findElement(By.name("user")).sendKeys("admin");
     wd.findElement(By.name("pass")).click();
- //   wd.findElement(By.name("pass")).clear();
+    //   wd.findElement(By.name("pass")).clear();
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+      }
+
+      @Test
+  public void testGroupCreation() {
     wd.findElement(By.linkText("groups")).click();
     wd.findElement(By.name("new")).click();
     wd.findElement(By.name("group_name")).click();
@@ -42,6 +44,18 @@ public class GroupCreationTests {
     wd.findElement(By.name("group_footer")).clear();
     wd.findElement(By.name("group_footer")).sendKeys("test6");
     wd.findElement(By.name("submit")).click();
-
   }
+  @AfterMethod
+   public void tearDown() { wd.quit(); }
+
+  public static boolean isAlertPresent(FirefoxDriver wd){
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
 }
+
