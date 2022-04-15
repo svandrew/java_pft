094a743edd7d6a4bcc83d2d9163d4cc2e4f0d5bf
svandrew <58271925+svandrew@users.noreply.github.com>
2022-04-15 13:48:28 +0300

    выделили вспомогательные методы
    каждое действие или группу действий через рефактор экстракт метод выделили в отдельный метод и задали им читаемые имена, для дальнейшего вызова

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.util.concurrent.TimeUnit;
 
 
@@ -17,6 +18,10 @@ public class GroupCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
+    login();
+  }
+
+  private void login() {
     wd.get("http://localhost/addressbook/group.php");
     wd.findElement(By.xpath("//input[@name='user']")).click();
     wd.findElement(By.xpath("//input[@name='user']")).sendKeys("admin");
@@ -24,6 +29,7 @@ public class GroupCreationTests {
     wd.findElement(By.name("pass")).sendKeys("secret");
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
+
   @After
   public void tearDown() {
     wd.quit();
@@ -31,9 +37,22 @@ public class GroupCreationTests {
 
   @Test
   public void testGroupCreation() {
+    gotoGroupPage();
+    initGroupCreation();
+    fillGroupForm();
+    submitGroupCreation();
+    returnToGroupPage();
+  }
 
-    wd.findElement(By.linkText("groups")).click();
-    wd.findElement(By.name("new")).click();
+  private void returnToGroupPage() {
+    wd.findElement(By.xpath("//a[contains(text(),'group page')]")).click();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm() {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
     wd.findElement(By.name("group_name")).sendKeys("test3");
@@ -43,8 +62,13 @@ public class GroupCreationTests {
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
     wd.findElement(By.name("group_footer")).sendKeys("test6");
-    wd.findElement(By.name("submit")).click();
-    wd.findElement(By.xpath("//a[contains(text(),'group page')]")).click();
+  }
 
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void gotoGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 }
