26a840f2d4f9c7d1d5e25b55e4c09a07ef431d8c
svandrew <58271925+svandrew@users.noreply.github.com>
2022-04-18 19:24:06 +0300

    завершет урок 2/11

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -3,21 +3,17 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SessionHelper {
-
-  private FirefoxDriver wd;
+public class SessionHelper extends HelperBase {
 
   public SessionHelper(FirefoxDriver wd) {
-    this.wd = wd;
+    super(wd);
   }
 
   public void login(String username, String password) {
     wd.get("http://localhost/addressbook/group.php");
-    wd.findElement(By.xpath("//input[@name='user']")).click();
-    wd.findElement(By.xpath("//input[@name='user']")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    type(By.xpath("//input[@name='user']"),username);
+    type(By.name("pass"),password);
+    click(By.xpath("//form[@id='LoginForm']/input[3]"));
   }
 }
   
